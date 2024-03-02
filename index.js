7770872971afcc828a04da09e9aeaf3ae4c0a379
Yousafkhan987 <160566044+Yousafkhan987@users.noreply.github.com>
2024-03-02 19:52:14 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+//Example.1 String
+var personname = "yousaf khan";
+console.log(personname);
+//Example.2 Number
+var personrollnumber = 27;
+console.log(personrollnumber);
+//Example.3 Boolean
+var isname = true;
+console.log(isname);
+//Example.4 Null
+var Null = null;
+console.log(null);
+//Example.5 Undefined
+var undefin = undefined;
+console.log(undefined);
---
+++ index.ts
@@ -0,0 +1,19 @@
+//Example.1 String
+let personname :string = "yousaf khan"
+console.log(personname)
+
+//Example.2 Number
+let personrollnumber :number =27
+console.log(personrollnumber) 
+
+//Example.3 Boolean
+let isname: boolean = true
+console.log(isname)
+
+//Example.4 Null
+let Null:null = null;
+console.log(null)
+
+//Example.5 Undefined
+let undefin:undefined = undefined;
+console.log(undefined)
